ee0b62fc348e4932f7378de045d848ea38873e44
superninjaXII <lunguj665@gmail.com>
2025-04-10 19:56:49 +0200

    added error handling

--- backend/controllers/createProduct.controller.ts
+++ backend/controllers/createProduct.controller.ts
@@ -4,11 +4,22 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const createProduct = async (req: Request, res: Response) => {
   const { name, description, images } = req.body;
-  const newProduct = await productModel.create({
-    id: uuidv4(),
-    name,
-    description,
-    images
-  })
-  res.send("product created")
+  if (name || description || images == "") {
+    res.status(400).send(" one of the inputs is empty")
+    return
+  }
+  try {
+    await productModel.create({
+      id: uuidv4(),
+      name,
+      description,
+      images
+    })
+
+    res.send("product created").status(201)
+  }
+  catch (err) {
+    console.error(err)
+    res.send("some problem occured while creating product").status(500)
+  }
 } 
